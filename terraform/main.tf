51dacc607f883dc95923a94deb29cb53675af65e
Blotjarl <marshall.junior03@gmail.com>
2025-09-02 09:33:08 -0500

    fix!: Final correction for Terraform outputs for CI/CD

--- terraform/main.tf
+++ terraform/main.tf
@@ -157,5 +157,5 @@ output "ecr_repository_url" {
 
 output "ecr_repository_name" {
   description = "The name of the ECR repository for the ETL container."
-  value       = aws_ecr_repository.etl_repo.name
+  value       = aws_ecr_repository.taxi_processing_repo.name
 }
